3bbdb420074746960313068e5f4ffabe9d4c265b
davimattos <davimattosdev@gmail.com>
2023-08-16 17:45:49 -0300

    adding a cell for title and descripton to table view

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Davi Mattos on 13/08/23.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
